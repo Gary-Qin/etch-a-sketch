cbfd5c21f02e657f83cb41b3e52323f0720b4468
Gary Qin <garyzhqin@gmail.com>
2024-05-09 20:36:55 -0400

    add key down feature

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ const container = document.querySelector(".container");
 const resetButton = document.querySelector(".reset");
 const dimensionButton = document.querySelector(".change-dimension");
 const dimensionSlider = document.querySelector("#size-input");
+const colourPicker = document.querySelector("#pen");
 const dimensionValue = document.querySelectorAll(".size");
 let tiles;
+
+let mouseDown = 0;
+document.body.onkeydown = function() { 
+    penDown(tiles, colourPicker.value)
+}
+document.body.onkeyup = function() {
+    penUp();
+}
 
 container.addEventListener("load", setGrid(16));
 resetButton.addEventListener("click", () => clearGrid());
 dimensionButton.addEventListener("click", () => removeGrid());
+colourPicker.addEventListener("input", (e) => {
+    console.log(e.target.value);
+    setColour(tiles, e.target.value);
+})
 
 dimensionValue.forEach(node => {node.textContent = dimensionSlider.value})
 dimensionSlider.addEventListener("input", (e) => {
@@ -17,13 +30,8 @@ dimensionSlider.addEventListener("input", (e) => {
 })
 
 function setGrid(num) {
-    console.log(num);
-
     const containerWidth = container.offsetWidth - 6;
     const containerHeight = container.offsetHeight - 6;
-
-    console.log(containerHeight);
-    console.log(containerWidth);
 
     for(let i = 0; i < num ** 2; i++) {
         const tile = document.createElement("div");
@@ -32,17 +40,31 @@ function setGrid(num) {
             `background-color:#FFFFFF;
             width:${containerWidth / num}px;
             height:${containerHeight / num}px;`);
-        tile.addEventListener("mouseover", () => {
-            tile.style.backgroundColor = "#0070F0";
-        })
-        tile.addEventListener("mouseout", () => {
-            tile.style.backgroundColor = "#0047F0";
-        })
         container.appendChild(tile);
     }
     tiles = document.querySelectorAll(".tile");
-    console.log(tiles);
 }
+
+function penDown(tiles, colour) {
+    tiles.forEach(tile => {
+        tile.addEventListener("mouseover", () => {
+            tile.style.backgroundColor = colour;
+        })
+    })
+    console.log("pen down");
+}
+
+function penUp() {
+    // tiles = document.querySelectorAll(".tile");
+    tiles.forEach(tile => {
+        const oldTile = tile;
+        const newTile = oldTile.cloneNode(true);
+        oldTile.parentNode.replaceChild(newTile, oldTile);
+    })
+    tiles = document.querySelectorAll(".tile");
+    console.log("pen up");
+}
+
 
 function clearGrid() {
     console.log("Reset");
